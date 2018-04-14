e1466be14612afd892f59ab599e05d28a426920d
Mechtai <32009627+Mechtai@users.noreply.github.com>
2018-04-14 17:08:29 +0300

    accordion

--- javascripts/O_MenuItem.js
+++ javascripts/O_MenuItem.js
@@ -1,12 +1,32 @@
 $(function() {
   $(".A_MenuItemButtom").click(function(e) {
+    e.preventDefault()
+
   let container = $(e.target).closest(".O_MenuItem");
+
+  let subItems = container.find(".A_SubMenuItem");
+
+  let item = subItems[0];
+  let height = 0;
+
+
+  if (item) {
+    let itemHeight = $(item).height()
+    height = subItems.length * itemHeight
+  };
+
+
   if (container.hasClass("active")) {
     container.removeClass("active")
+
+    $(".M_SubMenu").height(0);
   } else {
     $(".O_MenuItem").removeClass("active");
-    container.addClass("active");
-  }
+    $(".M_SubMenu").height(0);
 
+    container.addClass("active");
+
+    container.find(".M_SubMenu").height(height);
+    }
   });
 });
